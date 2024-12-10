0f07b412ae079e701d79c461449cb27adf86887d
bktang1 <166488376+bktang1@users.noreply.github.com>
2024-12-10 13:20:29 -0800

    Add files via upload

--- new_ui_test.py
+++ new_ui_test.py
@@ -62,9 +62,9 @@ def inject_custom_css():
 def welcome_page():
     #st.title(":blue[Stroke Risk Assessment and Personalized Health Plan]")
     st.markdown('<h1 style="text-align: center; font-family:Bubblegum Sans; font-size:30px; color:#1fe0c3;">Stroke Risk Assessment and Personalized Health Plan</h1>', unsafe_allow_html=True)
-    st.write("Understand your stroke risk and get personalized health plans.")
+    #st.write("Understand your stroke risk and get personalized health plans.")
     st.markdown(
-    '<p style="text-align: center; font-family: Arial; font-size: 18px;">Understand your stroke risk and get personalized health plans.</p>',
+    '<p style="text-align: center; font-family:Bubblegum Sans; font-size: 18px;">Understand your stroke risk and get personalized health plans.</p>',
     unsafe_allow_html=True
     )
     if st.button("Start Assessment"):
